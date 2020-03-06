f3dabdc6d3ffe0a9375f6552b204e06f5016e23a
corn-cloud <comowaiye@gmail.com>
2020-03-06 11:13:49 -0600

    code

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -4,4 +4,19 @@ class VendorsController < ApplicationController
     @vendors = Vendor.all
   end
 
+  def show
+    
+    @vendor = Vendor.find(params[:id]) 
+    @sweets = Sweet.all 
+  end
+
+
+
+  private
+
+  def vendor_params
+    params.require(:vendor).permit(:name)
+  end
+
+
 end
---
+++ app/models/vendorsweet.rb
@@ -0,0 +1,7 @@
+class Vendorsweet < ApplicationRecord
+    belongs_to :vendor
+    belongs_to :sweet
+
+    validates :price, numericality: {only_integer: true }
+
+end
